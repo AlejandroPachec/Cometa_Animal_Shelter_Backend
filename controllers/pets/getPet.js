6827f57be3273369215bf9346706caee7759fc90
Alejandro Pacheco Ferro <138246057+AlejandroPachec@users.noreply.github.com>
2023-11-01 21:31:38 +0100

    Finish getPet

--- controllers/pets/getPet.js
+++ controllers/pets/getPet.js
@@ -1,89 +1,76 @@
 const getPool = require('../../db/connectDB');
-const path = require('node:path');
 require('dotenv').config();
-const { UPLOADS_DIR } = require('../../config');
 const generateError = require('../../helpers/generateError');
 
-async function getProduct (req, res, next) {
-    try {
-        const pool = await getPool();
-        const { idProduct } = req.params;
+async function getPet (req, res, next) {
+  try {
+    const pool = await getPool();
+    const { idPet } = req.params;
 
-        const [productInfo] = await pool.query(
-            `SELECT
-                p.id AS product_id,
-                p.name AS product_name,
-                p.description AS product_description,
-                p.price AS product_price,
-                p.category AS product_category,
-                p.user_id AS seller_id,
-                p.state AS product_state,
-                p.availability AS availability,
-                p.created_at AS time,
-                u.first_name AS seller_first_name,
-                u.last_name AS seller_last_name,
-                u.avatar AS user_avatar,
-                u.city AS city,
-                ph.name AS product_photo,
-                AVG(r.stars) AS avg_review_stars
-            FROM
-                products AS p
-            JOIN
-                users AS u ON p.user_id = u.id
-            LEFT JOIN
-                reviews AS r ON r.user_seller_id = p.user_id
-            LEFT JOIN
-                product_photo AS ph ON p.id = ph.product_id
+    const [petInfo] = await pool.query(
+      `SELECT
+      p.pet_id,
+      p.name,
+      p.species,
+      p.sex,
+      p.weight,
+      STR_TO_DATE(p.estimated_birthdate, '%Y-%m-%d') AS birthdate,
+      p.breed,
+      p.status,
+      p.description,
+      p.date_added,
+      p.adoption_date,
+      p.created_at,
+      GROUP_CONCAT(pp.photo) AS pet_photos,
+      DATEDIFF(CURDATE(), p.estimated_birthdate)/365 AS age
+        FROM
+      pets AS p
+        LEFT JOIN
+      pet_photos AS pp ON p.pet_id = pp.pet_id
             WHERE
-                p.id = ?
+                p.pet_id = ?
             GROUP BY
-                p.id, p.user_id, ph.name
-    `, [idProduct]);
+                p.pet_id, pp.photo
+    `, [idPet]);
 
-        const [productPhotos] = await pool.query(
-            'SELECT name FROM product_photo WHERE product_id = ?', [idProduct]
-        );
+    const [petPhotos] = await pool.query(
+      'SELECT photo FROM pet_photos WHERE pet_id = ?', [idPet]
+    );
 
-        if (productInfo.length === 0) {
-            return next(generateError('El producto que buscas no existe', 400));
-        };
+    if (petInfo.length === 0) {
+      return next(generateError('La mascota que buscas no existe', 400));
+    };
 
-        const user = {
-            id: productInfo[0].seller_id,
-            first_name: productInfo[0].seller_first_name,
-            last_name: productInfo[0].seller_last_name,
-            avatar: productInfo[0].user_avatar,
-            city: productInfo[0].city
-        };
+    const petImages = petPhotos.map((photo) => ({
+      url: `${photo.photo}`
+    }));
 
-        const productImages = productPhotos.map((photo) => ({
-            url: `${photo.name}`
-        }));
+    const pet = {
+      id: petInfo[0].pet_id,
+      name: petInfo[0].name,
+      species: petInfo[0].species,
+      sex: petInfo[0].sex,
+      weight: petInfo[0].weight,
+      birthdate: petInfo[0].birthdate,
+      breed: petInfo[0].breed,
+      status: petInfo[0].status,
+      description: petInfo[0].description,
+      adoption_date: petInfo[0].adoption_date,
+      age: petInfo[0].age
+    };
 
-        const product = {
-            id: productInfo[0].product_id,
-            name: productInfo[0].product_name,
-            description: productInfo[0].product_description,
-            price: productInfo[0].product_price,
-            category: productInfo[0].product_category,
-            avg_review_stars: productInfo[0].avg_review_stars,
-            state: productInfo[0].product_state,
-            time: productInfo[0].time
-        };
+    res.status(200).send({
+      status: 'Ok',
+      message: 'Mascota encontrada',
+      data: {
+        pet,
+        petImages
+      }
+    });
 
-        res.status(200).send({
-            status: 'Ok',
-            message: 'Producto encontrado',
-            data: {
-                product,
-                user,
-                productImages
-            }
-        });
-
-    } catch (error) {
-        next(error);
-    }
+  } catch (error) {
+    next(error);
+  }
 }
 
-module.exports = getProduct;
+module.exports = getPet;
--- routes/petsRouter.js
+++ routes/petsRouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const getAllPets = require('../controllers/pets/getAllPets');
 const addPet = require('../controllers/pets/addPet');
-// const getProduct = require('../controllers/products/getProduct');
+const getPet = require('../controllers/pets/getPet');
 const authTeamMember = require('../middlewares/authTeamMember');
 
 const petsRouter = express.Router();
 
 petsRouter.get('/', getAllPets);
 petsRouter.post('/addPet', authTeamMember, addPet);
-// productsRouter.get('/:idPet', getPet);
+petsRouter.get('/:idPet', getPet);
 
 module.exports = petsRouter;
